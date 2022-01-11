c8cab0cdfb97f91ba130cde720dbde3d1922c2f2
qiaolei <1170494354@qq.com>
2022-01-11 17:55:05 +0800

    add domOnResize

--- index.js
+++ index.js
@@ -26,9 +26,68 @@ const significantNum = (num, precision = 4, toFix = 3, noNumRet) => {
     };
 };
 
+const domOnResize = (el, cb) => {
+    const callback = cb ?? (() => { })
+    if (!!el) {
+        // wrapper
+        const wrapperEl = document.createElement('div')
+        console.log(wrapperEl.style)
+        Object.assign(wrapperEl.style, {
+            height: '100%'
+            , opacity: '0'
+        })
+        const scrollWrapperStyle = {
+            width: '20%'
+            , height: '20%'
+            , overflow: 'scroll'
+            , position: 'absolute'
+            , left: '0'
+            , top: '0'
+        }
+        // scrollWrapper1 smaller
+        const scrollWrapperEl1 = document.createElement('div')
+        Object.assign(scrollWrapperEl1.style, scrollWrapperStyle)
+        const child1 = document.createElement('div')
+        Object.assign(child1.style, {
+            width: '300%'
+            , height: '300%'
+        })
+        scrollWrapperEl1.appendChild(child1)
+        scrollWrapperEl1.scrollTop = 1000
+        scrollWrapperEl1.scrollLeft = 1000
+
+        // scrollWrapper2 larger
+        const scrollWrapperEl2 = document.createElement('div')
+        Object.assign(scrollWrapperEl2.style, scrollWrapperStyle)
+        const child2 = document.createElement('div')
+        Object.assign(child2.style, {
+            width: '300px'
+            , height: '300px'
+        })
+        scrollWrapperEl2.appendChild(child2)
+        scrollWrapperEl2.scrollTop = 1000
+        scrollWrapperEl2.scrollLeft = 1000
+
+        scrollWrapperEl1.addEventListener('scroll', () => {
+            scrollWrapperEl2.scrollTop = 1000
+            scrollWrapperEl2.scrollLeft = 1000
+        })
+        scrollWrapperEl2.addEventListener('scroll', () => {
+            scrollWrapperEl1.scrollTop = 1000
+            scrollWrapperEl1.scrollLeft = 1000
+        })
+
+        wrapperEl.appendChild(scrollWrapperEl1)
+        wrapperEl.appendChild(scrollWrapperEl2)
+
+        el.appendChild(wrapperEl)
+    }
+}
+
 const funcs = {
     roundNum,
     significantNum,
+    domOnResize,
 };
 
 export default funcs;
